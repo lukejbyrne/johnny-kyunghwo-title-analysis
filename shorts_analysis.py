751dbaddec142a7b5924a39415d2b721bdeab8c4
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-02 12:25:26 +0000

    get video details

--- shorts_analysis.py
+++ shorts_analysis.py
@@ -4,10 +4,16 @@ import scrapetube as st
 CHANNEL_ID = "UCuG-IHDOVyVXaKV92x3jU5Q"
 
 videos = st.get_channel(CHANNEL_ID)
+videos_details = ([])
 
 # for all shorts on channel
 for video in videos:
-    print(video)
     # get titles, views, upload date, video length
+    title = video['title']['runs'][0]['text']
+    published = video['publishedTimeText']['simpleText']
+    length = video['lengthText']['simpleText']
+    views = video['viewCountText']['simpleText']
 
+    print(title)
+        
 # analyse for trend analysis of title structure and popular topics
