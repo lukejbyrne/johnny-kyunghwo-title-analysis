4f9f730c271679775244529118c3919f6e51a110
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-02 11:53:49 +0000

    get videos using scraptube api

--- shorts_analysis.py
+++ shorts_analysis.py
@@ -1,4 +1,13 @@
+import scrapetube as st
+
+# Channel ID from about -> share channel via https://www.youtube.com/@JohnnyKyunghwo
+CHANNEL_ID = "UCuG-IHDOVyVXaKV92x3jU5Q"
+
+videos = st.get_channel(CHANNEL_ID)
+
 # for all shorts on channel
+for video in videos:
+    print(video)
     # get titles, views, upload date, video length
 
 # analyse for trend analysis of title structure and popular topics
