d4e22f833c4f30dfe50a9a95bcc3b02c1e97680b
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-02 12:29:04 +0000

    add required video details to list

--- shorts_analysis.py
+++ shorts_analysis.py
@@ -1,19 +1,32 @@
 import scrapetube as st
 
+# To store info needed
+class video:
+    def __init__(self, title, published, length, views):
+        self.title = title
+        self.published = published
+        self.length = length
+        self.views = views
+
 # Channel ID from about -> share channel via https://www.youtube.com/@JohnnyKyunghwo
 CHANNEL_ID = "UCuG-IHDOVyVXaKV92x3jU5Q"
 
-videos = st.get_channel(CHANNEL_ID)
-videos_details = ([])
+st_videos = st.get_channel(CHANNEL_ID)
+
+# Initialise list to store required details
+video_details = []
 
 # for all shorts on channel
-for video in videos:
-    # get titles, views, upload date, video length
-    title = video['title']['runs'][0]['text']
-    published = video['publishedTimeText']['simpleText']
-    length = video['lengthText']['simpleText']
-    views = video['viewCountText']['simpleText']
+for v in st_videos:
 
-    print(title)
+    # get titles, views, upload date, video length
+    title = v['title']['runs'][0]['text']
+    published = v['publishedTimeText']['simpleText']
+    length = v['lengthText']['simpleText']
+    views = v['viewCountText']['simpleText']
+
+    video_details.append(video(title, published, length, views))
+
+    print(video_details[0].title)
         
 # analyse for trend analysis of title structure and popular topics
