13b2c4841314d91d2cc53e1b8c9798bdef45ff18
Luke Byrne <lukebyrnee97@gmail.com>
2024-02-02 13:01:10 +0000

    WIP: old one was longform vids, changing to using short

--- shorts_analysis.py
+++ shorts_analysis.py
@@ -2,31 +2,31 @@ import scrapetube as st
 
 # To store info needed
 class video:
-    def __init__(self, title, published, length, views):
+    def __init__(self, title, views):
         self.title = title
-        self.published = published
-        self.length = length
         self.views = views
 
 # Channel ID from about -> share channel via https://www.youtube.com/@JohnnyKyunghwo
 CHANNEL_ID = "UCuG-IHDOVyVXaKV92x3jU5Q"
 
-st_videos = st.get_channel(CHANNEL_ID)
+st_videos = st.get_channel(CHANNEL_ID,"https://www.youtube.com/@JohnnyKyunghwo","JohnnyKyunghwo",550,1, "newest", "shorts")
 
 # Initialise list to store required details
 video_details = []
+
+print(list(st_videos)[0])
 
 # for all shorts on channel
 for v in st_videos:
 
     # get titles, views, upload date, video length
-    title = v['title']['runs'][0]['text']
-    published = v['publishedTimeText']['simpleText']
-    length = v['lengthText']['simpleText']
+    title = v['headline']['simpleText']['headline']['simpleText']
     views = v['viewCountText']['simpleText']
 
-    video_details.append(video(title, published, length, views))
+    video_details.append(video(title, views))
 
-    print(video_details[0].title)
+    print(views)
+    # print(views)
+    # print(video_details[0])
         
 # analyse for trend analysis of title structure and popular topics
